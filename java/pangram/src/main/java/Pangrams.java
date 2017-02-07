4c8da552a73d1b847e02c63a8d75653a81b438f9
Fellipe Souto <fsampaio@avenuecode.com>
2017-02-07 10:56:40 -0200

    Improvements from code review

--- java/pangram/src/main/java/Pangrams.java
+++ java/pangram/src/main/java/Pangrams.java
@@ -1,34 +1,25 @@
 import java.util.Arrays;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Collection;
 
 public class Pangrams {
 
   private static final List<String> alphabet = Arrays.asList("abcdefghijklmnopqrstuvwxyz".split(""));
 
   public static boolean isPangram(String sentence) {
-    if(invalidSentence(sentence)) return false ;
-
-    Set<String> letterSet = new HashSet<String>();
-    for(String letter : sanitize(sentence).split("")) {
-      letterSet.add(letter);
-    }
-
-    return hasAllLetters(letterSet);
+    if(hasInvalidSentence(sentence)) return false ;
+    Set<String> letterSet = new HashSet<String>(Arrays.asList(sanitize(sentence).split("")));
+    return hasSameSize(letterSet, alphabet);
   }
 
-  private static boolean invalidSentence(String sentence) {
+  private static boolean hasInvalidSentence(String sentence) {
     return (sentence == null || sentence.length() < alphabet.size());
   }
 
-  private static boolean hasAllLetters(Set<String> letterSet) {
-    if(letterSet.size() == alphabet.size()) {
-      return true;
-    } else {
-      return false;
-    }
+  private static boolean hasSameSize(Collection<String> col1, Collection<String> col2) {
+    return col1.size() == col2.size();
   }
 
   private static String sanitize(String str) {
